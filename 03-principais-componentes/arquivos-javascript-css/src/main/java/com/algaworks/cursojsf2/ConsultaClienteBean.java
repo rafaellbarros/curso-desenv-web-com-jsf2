2f7c6ebbc3bcb189d985050c7fb266feb5be184a
Rafael Barros <rafaelbarros.df@gmail.com>
2020-06-25 02:13:03 -0300

    :construction: Arquivos javascript e css

---
+++ 03-principais-componentes/arquivos-javascript-css/src/main/java/com/algaworks/cursojsf2/ConsultaClienteBean.java
@@ -0,0 +1,27 @@
+package com.algaworks.cursojsf2;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
+public class ConsultaClienteBean {
+
+    private List<Cliente> clientes = new ArrayList<>();
+
+    @PostConstruct
+    public void consultar() {
+        this.getClientes().add(new Cliente(1, "Pedro Pereira", "Uberlândia"));
+        this.getClientes().add(new Cliente(2, "José da Silva", "Araguari"));
+        this.getClientes().add(new Cliente(4, "Joaquim Manoel", "Uberaba"));
+        this.getClientes().add(new Cliente(6, "Sebastião Souza", "São Paulo"));
+        this.getClientes().add(new Cliente(7, "Joana Carvalho", "Uberlândia"));
+        this.getClientes().add(new Cliente(10, "Maria José", "Rio de Janeiro"));
+    }
+
+    public List<Cliente> getClientes() {
+        return this.clientes;
+    }
+
+}
